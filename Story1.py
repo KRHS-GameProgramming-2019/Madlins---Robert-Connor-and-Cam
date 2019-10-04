467addc26e3ac1e79be734bea64bdc30ee2b4e54
Robert Needham <Robert.Needham@kearsarge.org>
2019-10-04 11:34:33 -0400

    story1

--- Story1.py
+++ Story1.py
@@ -7,14 +7,24 @@ def Story1(debug = False):
     friendName1 = getWord("Enter a name: ", debug)
     sport1 = getSport("Enter a Sport: ", debug)
     game = getWord ("Type an Xbox game: ", debug)
+    snack = getWord("Type a snack: ", debug)
     
     out = "\n"
     out += "One day me and my friend, " + friendName1
     out += " were out playing " + sport1
-    out += "my freind got tired" + freindName1
+    out += "my freind got tired" + friendName1
     out += "my freind and I went over my house"+ friendName1
     out += "we played Xbox"+ game
-    
+    out += "he lost and I won"
+    out += "we got a quick snack to eat"+ snack
+    out += "we went back outside and played" + sport1
+    out += "my freind decided to sleep over"
+    out += "we pulled an all nighter"
+    out += "during the night we played chopped"
+    out += "the loser had too jump in the cold pond "
+    out += "he ended up winning as my dish was terrible"
+    out += "because I lost i had to jump in the pond"
+    out += "the next day my freind went home and I had to get ready for a football game"
     
     
     return out
