197796a2fea1e8166c55552f1e810cac599d36f5
Robert Needham <Robert.Needham@kearsarge.org>
2019-10-07 11:29:09 -0400

    story1

--- Story1.py
+++ Story1.py
@@ -12,19 +12,19 @@ def Story1(debug = False):
     out = "\n"
     out += "One day me and my friend, " + friendName1
     out += " were out playing " + sport1
-    out += "my freind got tired" + friendName1
-    out += "my freind and I went over my house"+ friendName1
-    out += "we played Xbox"+ game
-    out += "he lost and I won"
-    out += "we got a quick snack to eat"+ snack
-    out += "we went back outside and played" + sport1
-    out += "my freind decided to sleep over"
-    out += "we pulled an all nighter"
-    out += "during the night we played chopped"
-    out += "the loser had too jump in the cold pond "
-    out += "he ended up winning as my dish was terrible"
-    out += "because I lost i had to jump in the pond"
-    out += "the next day my freind went home and I had to get ready for a football game"
+    out += + freindName1 + " got tired " + friendName1
+    out += " freindName and I went over my house "+ friendName1
+    out += " we played Xbox "+ game
+    out += " he lost and I won "
+    out += "we got " + snack + " to eat"
+    out +=  "we went back outside and played " + sport1
+    out += " my freind decided to sleep over "
+    out += " we pulled an all nighter "
+    out += " during the night we played chopped "
+    out += " the loser had too jump in the cold pond "
+    out += " he ended up winning as my dish was terrible "
+    out += " because I lost i had to jump in the pond "
+    out += " the next day my freind went home and I had to get ready for a football game "
     
     
     return out
