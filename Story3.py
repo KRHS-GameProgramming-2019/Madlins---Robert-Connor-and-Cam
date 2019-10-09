bd2eff1f7db4c1cec56d51cdb95981e0a557e6f9
Robert Needham <Robert.Needham@kearsarge.org>
2019-10-09 11:36:00 -0400

    story3

---
+++ Story3.py
@@ -0,0 +1,26 @@
+from Getters import *
+
+def Story1(debug = False):
+    if debug: print("Story1 Function")
+
+    print("\n")
+    sport1 = getSport("Enter a Sport: ", debug)
+    
+     out = "\n"
+    out += ""
+    out += ""
+    out += ""
+    out += ""
+    out += ""
+    out += ""
+    out += ""
+    out += "" 
+    out += ""
+    out += ""
+    out += ""
+    out += ""
+    out += ""
+    out += ""
+    out += ""
+    
+    
