6854d602465ed13f5ec1fb23fa6fc2ecafa055f2
connor pillsbury <connor.pillsbury@kearsarge.org>
2019-10-09 11:26:39 -0400

    story

---
+++ Story2.py
@@ -0,0 +1,11 @@
+ from Getters import *
+
+def Story1(debug = False):
+    if debug: print("Story1 Function")
+
+    print("\n")
+    friendName1 = getWord("Enter a name: ", debug)
+    sport1 = getSport("Enter a Sport: ", debug)
+    game = getWord ("Type an Xbox game: ", debug)
+    snack = getWord("Type a snack: ", debug)
+    
